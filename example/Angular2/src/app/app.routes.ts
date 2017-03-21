a8fcce6b41283d76efdf679d4769420f5e5c9bf3
pulse <tuan.cao@tekcent.com>
2017-03-21 17:40:41 +0700

    update code.

--- example/Angular2/src/app/app.routes.ts
+++ example/Angular2/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 export const rootRouterConfig: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: AppComponent },
 ];
 
